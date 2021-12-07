6112c175114913da8b7d1924fb352f6dc7e25e02
Anna Dowhower <anna.dowhower@att.net>
2021-12-06 21:47:22 -0600

    added all necessary windows and buttons

---
+++ Prototype/Prototype/wndHousehold.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prototype
+{
+    /// <summary>
+    /// Interaction logic for wndHousehold.xaml
+    /// </summary>
+    public partial class wndHousehold : Window
+    {
+        public wndHousehold()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegisterHousehold_Click(object sender, RoutedEventArgs e)
+        {
+            wndRegisterHousehold wnd21 = new wndRegisterHousehold();
+            wnd21.Show();
+        }
+
+        private void btnViewHouseholdInfo_Click(object sender, RoutedEventArgs e)
+        {
+            wndViewHouseholdInfo wnd22 = new wndViewHouseholdInfo();
+            wnd22.Show();
+        }
+    }
+}
---
+++ Prototype/Prototype/wndRegisterHousehold.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Prototype
+{
+    /// <summary>
+    /// Interaction logic for wndRegisterHousehold.xaml
+    /// </summary>
+    public partial class wndRegisterHousehold : Window
+    {
+        public wndRegisterHousehold()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAddMember_Click(object sender, RoutedEventArgs e)
+        {
+            wndAddMember wnd211 = new wndAddMember();
+            wnd211.Show();
+        }
+    }
+}
